65b7d5701340128c6696823c39f25baa5a9aabfc
Zeb Sampedro <sampedro@colorado.edu>
2015-03-10 16:02:06 -0600

    State can now be passed-by-reference properly.

--- client/oide/components/states/states.js
+++ client/oide/components/states/states.js
@@ -2,13 +2,14 @@
 
 angular.module('oide.states', [])
 .factory('StateService', ['$http', '$log', function($http,$log) {
+  var state = {'state': {}};
   var storeState = function () {
     $http({
       url: '/a/state',
       method: 'POST',
       params: {
         _xsrf: getCookie('_xsrf'),
-        state: JSON.stringify(state)
+        state: JSON.stringify(state.state)
       }
     })
     .success(function (data, status, headers, config) {
@@ -29,9 +30,20 @@ angular.module('oide.states', [])
       $log.error('Failed to retrieve state for user.');
     });
   };
-  var state = getState();
+  var initializeState = function () {
+    return $http
+    .get('/a/state')
+    .success(function (data, status, headers, config) {
+      $log.debug('Retrieved state for user: ', data);
+      state.state = data;
+    })
+    .error(function (data, status, headers, config) {
+      $log.error('Failed to retrieve state for user.');
+    });
+  };
+  initializeState();
   return {
-    state: state,
+    state: state.state,
     storeState: function () {
       storeState();
     },
