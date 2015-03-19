a8aa27fc21d1363f793ecff7051eba6bfacdfaba
Zeb Sampedro <sampedro@colorado.edu>
2015-03-19 14:40:13 -0600

    Added framework for VNC module.

---
+++ client/oide/vnc/vnc_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('oide.vnc module', function() {
+
+  beforeEach(module('oide.vnc'));
+
+  describe('vnc controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var vncCtrl = $controller('VncCtrl');
+      expect(vncCtrl).toBeDefined();
+    }));
+
+  });
+});
