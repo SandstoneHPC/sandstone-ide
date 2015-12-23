548140e7fec0e023f5ac96b568fd23402e6d3727
Saurabh Sood <saurabh.sood@colorado.edu>
2015-12-23 10:02:40 -0700

    ui improvements - visual cue for edit

--- oide/apps/filebrowser/static/filebrowser.controller.js
+++ oide/apps/filebrowser/static/filebrowser.controller.js
@@ -351,7 +351,23 @@ angular.module('oide.filebrowser')
     });
   };
 }])
-
+.directive('syncFocusWith', function($timeout, $rootScope) {
+    return {
+        restrict: 'A',
+        scope: {
+            focusValue: "=syncFocusWith"
+        },
+        link: function($scope, $element, attrs) {
+            $scope.$watch("focusValue", function(currentValue, previousValue) {
+                if (currentValue === true && !previousValue) {
+                    $element[0].focus();
+                } else if (currentValue === false && previousValue) {
+                    $element[0].blur();
+                }
+            })
+        }
+    }
+})
 .controller('DeleteModalInstanceCtrl', ['FilesystemService', '$modalInstance', 'selectedFile',function (FilesystemService, $modalInstance, selectedFile) {
   var self = this;
   self.selectedFile = selectedFile;
