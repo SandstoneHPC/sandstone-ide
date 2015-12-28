21a42070878e473c94a4738691c8559c15a3cfb0
saurabhsood91 <saurabhsood91@gmail.com>
2015-12-28 11:30:38 -0700

    make uploads work

--- oide/apps/filebrowser/handlers.py
+++ oide/apps/filebrowser/handlers.py
@@ -6,6 +6,7 @@ import logging
 import operator
 import tornado.web
 import tornado.ioloop
+import tempfile
 
 from tornado.concurrent import Future
 from tornado import gen
@@ -174,6 +175,56 @@ class FilesystemUtilHandler(BaseHandler,FSMixin):
         self.fs.change_permisions(filepath, perm_string)
         result = "Changed permission of ", filepath, " to ", perm_string
         return result
+
+@tornado.web.stream_request_body
+class SimpleUploadHandler(BaseHandler, FSMixin):
+
+    @tornado.web.authenticated
+    def post(self):
+        fp = self.request.headers['Uploaddir']
+        dest_path = os.path.join(fp,self.filename)
+        self.fd.close()
+        # shutil.move(self.fd.name,dest_path)
+        self.fs.move_file(self.fd.name, dest_path)
+
+    @tornado.web.authenticated
+    def prepare(self):
+        self.tmp_cache = ''
+        self.stream_started = False
+        self.request.connection.set_max_body_size(2*1024**3)
+        fd_info = tempfile.mkstemp()
+        self.fd = open(fd_info[1],'w')
+
+    def data_received(self, data):
+        self.tmp_cache += data
+        pdata = self._process(data)
+        self.fd.write(pdata)
+
+    def _process(self, data):
+        trimmed = data.splitlines()
+        tmp = data.splitlines(True)
+
+        if not self.stream_started:
+            self.boundary = trimmed[0].strip()
+            tmp = tmp[1:]
+            trimmed = trimmed[1:]
+            self.stream_started = True
+
+            try:
+                first_elem = trimmed[:5].index("")
+                metadata = trimmed[:first_elem]
+                self.filename = metadata[0].split(';')[-1].split('=')[-1][1:-1]
+                tmp = tmp[first_elem + 1:]
+                trimmed = trimmed[first_elem + 1:]
+            except ValueError:
+                pass
+
+        try:
+            last_elem = trimmed.index(self.boundary + "--")
+            self.stream_started = False
+            return "".join(tmp[:last_elem - 1])
+        except ValueError:
+            return "".join(tmp)
 
 class FilesystemUploadHandler(BaseHandler,FSMixin):
 
