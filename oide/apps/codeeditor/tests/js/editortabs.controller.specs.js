0242307a9951b7e6e332a00cbb313cef0b451b6f
Saurabh Sood <saurabh.sood@colorado.edu>
2016-02-05 12:26:57 -0700

    add specs for editor tabs controller

--- oide/apps/codeeditor/tests/js/editortabs.controller.specs.js
+++ oide/apps/codeeditor/tests/js/editortabs.controller.specs.js
@@ -44,5 +44,53 @@ describe('oide.editor tabs', function(){
       scope.ctrl.openDocument('/home/saurabh/file1');
       expect($editorService.openDocument).toHaveBeenCalled();
     });
+    it('should save document', function(){
+      httpBackend.flush();
+      spyOn($editorService, 'saveDocument');
+      scope.ctrl.saveDocument({filepath: '/home/saurabh/file1'});
+      expect($editorService.saveDocument).toHaveBeenCalled();
+    });
+    it('should undo changes', function(){
+      httpBackend.flush();
+      spyOn($editorService, 'undoChanges');
+      scope.ctrl.undoChanges({filepath: '/home/saurabh/file1'});
+      expect($editorService.undoChanges).toHaveBeenCalled();
+    });
+    it('should redo changes', function(){
+      httpBackend.flush();
+      spyOn($editorService, 'redoChanges');
+      scope.ctrl.redoChanges({filepath: '/home/saurabh/file1'});
+      expect($editorService.redoChanges).toHaveBeenCalled();
+    });
+    it('should copy selection', function(){
+      httpBackend.flush();
+      spyOn($editorService, 'copySelection');
+      scope.ctrl.copySelection();
+      expect($editorService.copySelection).toHaveBeenCalled();
+    });
+    it('should cut selection', function(){
+      httpBackend.flush();
+      spyOn($editorService, 'cutSelection');
+      scope.ctrl.cutSelection();
+      expect($editorService.cutSelection).toHaveBeenCalled();
+    });
+    it('should paste to clipboard', function(){
+      httpBackend.flush();
+      spyOn($editorService, 'pasteClipboard');
+      scope.ctrl.pasteClipboard();
+      expect($editorService.pasteClipboard).toHaveBeenCalled();
+    });
+    it('should comment selection', function(){
+      httpBackend.flush();
+      spyOn($editorService, 'commentSelection');
+      scope.ctrl.commentSelection();
+      expect($editorService.commentSelection).toHaveBeenCalled();
+    });
+    it('should open search box', function(){
+      httpBackend.flush();
+      spyOn($editorService, 'openSearchBox');
+      scope.ctrl.openSearchBox();
+      expect($editorService.openSearchBox).toHaveBeenCalled();
+    });
   });
 });
