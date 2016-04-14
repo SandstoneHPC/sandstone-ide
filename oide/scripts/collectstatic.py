62bdb727a6a942450bb7ca54ba0adbd1ea76140f
saurabhsood91 <saurabhsood91@gmail.com>
2016-04-14 16:22:27 -0600

    copy karma.conf.js and core components

--- oide/scripts/collectstatic.py
+++ oide/scripts/collectstatic.py
@@ -21,10 +21,16 @@ for app_name in global_settings.INSTALLED_APPS:
 # print spec_list
 for app in spec_list:
     # copy static files to the devtest directory
-    shutil.copytree(app[1] + '/static', "../devtest/" + app[0] + "/static")
+    shutil.copytree(app[1] + '/static', "../devtest/apps/" + app[0] + "/static")
     if os.path.exists(app[1] + '/tests'):
-        shutil.copytree(app[1] + '/tests', "../devtest/" + app[0] + "/tests")
+        shutil.copytree(app[1] + '/tests', "../devtest/apps/" + app[0] + "/tests")
 
 # copy node_modules and bower_components
 shutil.copytree('../client/node_modules', '../devtest/node_modules')
 shutil.copytree('../client/oide/bower_components', '../devtest/bower_components')
+
+# copy core components
+shutil.copytree('../client/oide/components', '../devtest/components')
+
+# copy karma.conf.js
+shutil.copy('../client/karma.conf.js', '../devtest')
