00223e7b2ccc981ea0bec61ee7d8d94b20696f85
Zebula Sampedro <sampedro@colorado.edu>
2016-05-03 14:58:10 -0600

    Added tests for BaseHandler.

--- oide/lib/tests/python/test_handlers.py
+++ oide/lib/tests/python/test_handlers.py
@@ -40,3 +40,25 @@ class MainHandlerTestCase(TestHandlerBase):
         self.assertEqual(response.code, 200)
         login_form = """<form action="/auth/login" method="POST" class="form-signin" role="form">"""
         self.assertIn(login_form,response.body)
+
+class BaseHandlerTestCase(TestHandlerBase):
+    @mock.patch.object(BaseHandler,'get_secure_cookie',return_value=EXEC_USER)
+    def test_get_current_user_authed(self,m):
+        request = mock.Mock()
+        handler = BaseHandler(self.get_app(),request)
+        user = handler.get_current_user()
+        self.assertEqual(user,EXEC_USER)
+
+    def test_get_current_user_unauthed(self):
+        request = mock.Mock()
+        request.cookies = None
+        handler = BaseHandler(self.get_app(),request)
+        user = handler.get_current_user()
+        self.assertEqual(user, None)
+
+    def test_get_template_path(self):
+        app = self.get_app()
+        request = mock.Mock()
+        handler = BaseHandler(app,request)
+        tpath = handler.get_template_path()
+        self.assertEqual(tpath,app.settings['project_dir'])
