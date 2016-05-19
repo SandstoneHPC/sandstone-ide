732b3c759653f2e6ec21eb72b87fce4a2cbda1fd
Zebula Sampedro <sampedro@colorado.edu>
2016-05-19 13:53:52 -0600

    Finished rewriting editor E2E specs.

--- oide/apps/codeeditor/tests/e2e/editor.spec.js
+++ oide/apps/codeeditor/tests/e2e/editor.spec.js
@@ -58,14 +58,6 @@ describe('OIDE Editor Tabs', function() {
       browser.sleep(3000);
     });
 
-    // it('should be marked unsaved after edit', function() {
-    //   element.all(by.css('#editor-nav-tabs li.ng-isolate-scope tab-heading > span')).then(function(arr) {
-    //     // Force a digest cycle.
-    //     browser.actions().mouseMove(element(by.css('#editor-nav-tabs li.ng-isolate-scope tab-heading'))).perform();
-    //     expect(arr[2].getAttribute('class')).toContain('fa-circle-o');
-    //   });
-    // });
-
     it('should have the entered text', function() {
       $('div.ace_content').getText().then(function(text) {
         expect(text.slice(0,-1)).toBe('test.');
@@ -119,53 +111,38 @@ describe('OIDE Editor Tabs', function() {
       // Create a new tab
       var initialTabCount = 0;
       var finalTabCount = 0;
-      var driver = browser.driver;
-
       // Save value of editor
-      var tab_1_text = "";
-      element(by.css('div.ace_content')).getText().then(function(text) {
-        tab_1_text = text;
+      var tabOneText = '';
+      $('div.ace_content').getText().then(function(text) {
+        tabOneText = text;
       });
-
-      driver.findElements(by.css('ul.nav-tabs > li > a')).then(function(elements){
-        initialTabCount = elements.length - 1;
-        driver.executeScript("arguments[0].click()", elements[1]).then(function(){
-          // Get new tab count
-          $$('ul.nav-tabs > li > a').then(function(elements){
-            var tabs = elements;
-            // Final Tab Count should be element length - 1 to leave out the 'plus' element
-            finalTabCount = elements.length - 1;
-            // expect finalTabCount to be 1 more than initial tab count
-            expect(finalTabCount).toBe(initialTabCount + 1);
-
-            // Send some text to Editor
-            sendKeys();
-
-            // Save value of tab 2
-            var tab_2_text = "";
-            element(by.css('div.ace_content')).getText().then(function(text){
-              tab_2_text = text;
-
-              // Switch to Tab 1
-              tabs[0].click().then(function(){
-                // Get value of ace editor
-                element(by.css('div.ace_content')).getText().then(function(text){
-                  //Expect text to be equal to tab_1_text
-                  expect(text).toBe(tab_1_text);
-
-                  // Switch to Tab 1
-                  tabs[1].click().then(function(){
-                    // Get value of editor
-                    element(by.css('div.ace_content')).getText().then(function(text){
-                      // Expect text to be equal to tab_2_text
-                      expect(text).toBe(tab_2_text);
-                    });
-                  });
-                });
-              });
-            });
-
-          });
+      // Get new tab count
+      initialTabCount = $$('ul.nav-tabs > li > a').count() - 1;
+      $$('ul.nav-tabs > li > a').get(1).click(function() {
+        // Final Tab Count should be element length - 1 to leave out the 'plus' element
+        finalTabCount = $$('ul.nav-tabs > li > a').count() - 1;
+        // expect finalTabCount to be 1 more than initial tab count
+        expect(finalTabCount).toBe(initialTabCount+1);
+      });
+      // Send some text to Editor
+      sendKeys();
+      // Save value of tab 2
+      var tabTwoText = '';
+      $('div.ace_content').getText().then(function(text) {
+        tabTwoText = text;
+      });
+      // Switch to Tab 1
+      $$('ul.nav-tabs > li > a').get(0).click(function() {
+        $('div.ace_content').getText().then(function(text) {
+          //Expect text to be equal to tabOneText
+          expect(text).toBe(tabOneText);
+        });
+      });
+      // Switch to Tab 2
+      $$('ul.nav-tabs > li > a').get(1).click(function() {
+        $('div.ace_content').getText().then(function(text) {
+          //Expect text to be equal to tabTwoText
+          expect(text).toBe(tabTwoText);
         });
       });
     });
@@ -197,39 +174,25 @@ describe('OIDE Editor Tabs', function() {
       // Save the value of editor text
       var initialText = "";
       var finalText = "";
-      var driver = browser.driver;
-      element(by.css('div.ace_content')).getText().then(function(text){
+
+      $('div.ace_content').getText().then(function(text) {
         initialText = text;
-        // console.log(initialText);
         // Send some more text
-        sendKeys("Hello World");
-
-        browser.sleep(2000).then(function(){
-          // Get new text
-          element(by.css('div.ace_content')).getText().then(function(text){
+        $('textarea.ace_text-input').sendKeys('Hello World').then(function() {
+          $('div.ace_content').getText().then(function(text) {
             finalText = text;
-
-            // Click on Undo
-            driver.findElements(by.css('span > .dropdown-menu > li > a')).then(function(elements){
-              driver.executeScript("arguments[0].click()", elements[0]).then(function(){
-                // Get the Text
-                // browser.pause();
-                element(by.css('div.ace_content')).getText().then(function(text){
-                  // Expect text to be equal to initialText
-                  expect(text.slice(0, -1)).toBe("");
-
-                  // Click on Redo
-                  driver.executeScript("arguments[0].click()", elements[1]).then(function(){
-                    element(by.css('div.ace_content')).getText().then(function(text){
-                      // Expect text to be equal to finalText
-                      expect(text).toBe(finalText);
-                    });
-                  });
-                });
-              });
-            });
           });
         });
+      });
+      // Click on Undo
+      $('span.dropdown-toggle').click();
+      $$('span > .dropdown-menu > li > a').get(0).click(function() {
+        expect($('div.ace_content').getText().slice(0,-1)).toBe(initialText);
+      });
+      // Click on Redo
+      $('span.dropdown-toggle').click();
+      $$('span > .dropdown-menu > li > a').get(1).click(function() {
+        expect($('div.ace_content').getText().slice(0,-1)).toBe(finalText);
       });
     });
 
