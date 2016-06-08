15c293d82120929886653acf1f897911e6402242
Zebula Sampedro <sampedro@colorado.edu>
2016-06-08 11:27:10 -0600

    Filebrowser e2e tests restructured to use PageObjects.

--- oide/apps/filebrowser/tests/e2e/filebrowser.spec.js
+++ oide/apps/filebrowser/tests/e2e/filebrowser.spec.js
@@ -7,58 +7,105 @@ var classMatcherHelper = function(element, cls) {
   });
 };
 
-describe('Filetree', function() {
-  beforeEach(function() {
+var FilebrowserPage = function() {
+  var progressBar = element(by.css('.progress-bar'));
+  var breadcrumbs = element.all(by.css('ol.breadcrumb > li'));
+  var dirDetails = element.all(by.css('#dir-detail-table > tbody'));
+  var files = element.all(by.css('.filetable-filename'));
+
+  this.get = function() {
     browser.get('/#/filebrowser');
+    // browser.waitForAngular();
+  };
+  this.getVolumeUsage = function() {
+    return progressBar.getAttribute('aria-valuenow');
+  };
+  this.breadcrumbs = function() {
+    return breadcrumbs;
+  };
+  this.dirDetails = function() {
+    return dirDetails.get(0);
+  };
+  this.files = function() {
+    return files;
+  };
+};
+
+var Filetree = function() {
+  var dirIcons = element.all(by.css('.tree-branch-head'));
+  var dirLabels = element.all(by.css('.tree-label'));
+  var selectedDirs = element.all(by.css('.tree-selected'));
+
+  this.dirIcons = function() {
+    return dirIcons;
+  };
+  this.dirLabels = function() {
+    return dirLabels;
+  };
+  this.selectedDirs = function() {
+    return selectedDirs;
+  };
+  this.selectFirstDir = function() {
+    dirLabels.first().click();
+  },
+  this.expandFirstDir = function() {
+    dirIcons.first().click();
+  };
+  this.expandDir = function(index) {
+    dirIcons.get(index).click();
+  };
+  this.selectDir = function(index) {
+    dirLabels.get(index).click();
+  };
+};
+
+describe('Filebrowser', function() {
+  var page, filetree;
+
+  beforeEach(function() {
+    page = new FilebrowserPage();
+    filetree = new Filetree();
+    page.get();
   });
 
   it('should open the filetree when a node is clicked', function(){
-    // Get the first filetree node
-    var fileNode = $$('.tree-branch-head').first();
-    // The element should have class fa-folder
-    expect(classMatcherHelper(fileNode, 'fa-folder')).toBeTruthy();
+    // The first element should have class fa-folder
+    expect(classMatcherHelper(filetree.dirIcons().first(),'fa-folder'))
+      .toBeTruthy();
     // Simulate click for the element
-    fileNode.click();
+    filetree.expandFirstDir();
     // The element should now have class fa-folder-open
-    expect(classMatcherHelper(fileNode, 'fa-folder-open')).toBeTruthy();
+    expect(classMatcherHelper(filetree.dirIcons().first(),'fa-folder-open'))
+      .toBeTruthy();
     // On clicking again, the class should not be set to fa-folder-open
-    fileNode.click();
-    expect(classMatcherHelper(fileNode, 'fa-folder-open')).toBeFalsy();
+    filetree.expandFirstDir();
+    expect(classMatcherHelper(filetree.dirIcons().first(),'fa-folder-open'))
+      .toBeFalsy();
   });
 
   it('should start with no volume selected', function() {
-    expect($$('.tree-selected').count()).toBe(0);
-    expect($('.progress-bar').getAttribute('aria-valuenow')).toBe('');
-    expect($$('ol.breadcrumb > li').count()).toBe(0);
-    expect($$('#dir-detail-table > tbody').get(0).$$('tr').count()).toBe(0);
+    expect(filetree.selectedDirs().count()).toBe(0);
+    expect(page.getVolumeUsage()).toBe('');
+    expect(page.breadcrumbs().count()).toBe(0);
+    expect(page.dirDetails().$$('tr').count()).toBe(0);
   });
 
   it('should populate page with volume info on selection', function() {
-    $$('.tree-label').get(1).click(function() {
-      expect($$('.tree-selected').count()).toBe(1);
-      expect($('.progress-bar').getAttribute('aria-valuenow') >= 0).toBeTruthy();
-      expect($$('ol.breadcrumb > li').count() > 0).toBeTruthy();
-      expect($$('#dir-detail-table > tbody').get(0).$$('tr').count() > 0).toBeTruthy();
+    filetree.dirLabels().first().click(function() {
+      expect(filetree.selectedDirs().count()).toBe(1);
+      expect(page.getVolumeUsage() >= 0).toBeTruthy();
+      expect(page.breadcrumbs().count() > 0).toBeTruthy();
+      expect(page.dirDetails().$$('tr').count() > 0).toBeTruthy();
     });
   });
-});
-
-describe('Tableview and File Details', function() {
-  // beforeEach(function() {
-    browser.get('/#/filebrowser');
-    browser.sleep(3000);
-  // });
 
   it('should allow files to be created and deleted', function() {
     // Select a folder to add/delete from
-    $$('.tree-label').first().click(function() {
-      var driver = browser.driver;
-      var initialFileCnt = $$('#dir-detail-table i.fa.fa-file').count()
+    filetree.dirLabels().last().click(function() {
       driver.findElements(by.css('#create-file-btn')).then(function(elements){
         driver.executeScript("arguments[0].click()", elements[0]).then(function() {
-          var fileList = $$('#dir-detail-table i.fa.fa-file');
           // file should be created in the selected directory
-          expect(fileList.count()).toBe(initialFileCnt+1212);
+          expect(page.files().count()).toBe(13);
           $$('.filetable-filename').last().click(function() {
             // File should be selected now
             expect(classMatcherHelper($$('.filetable-filename').last(),'selected'))
@@ -66,47 +113,7 @@ describe('Tableview and File Details', function() {
             // Delete file
             driver.findElements(by.css('#delete-file-btn')).then(function(elements){
               driver.executeScript("arguments[0].click()", elements[0]).then(function() {
-                var fileList = $$('#dir-detail-table i.fa.fa-file');
-                expect(fileList.count()).toBe(initialFileCnt);
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-
-  it('should allow files directories to be created and deleted',function() {
-    // Select a folder to add/delete from
-    $$('.tree-branch-head').first().click(function() {
-      var initialHeadCnt = $$('.tree-branch-head').count();
-      $$('.tree-label').first().click(function() {
-        var initialDirCnt = $$('#dir-detail-table i.fa.fa-folder').count();
-        $('#create-dir-btn').click(function() {
-          var dirList = $$('#dir-detail-table i.fa.fa-folder');
-          // Directory should show in directory detail table
-          expect(dirList.count()).toBe(initialDirCnt+1);
-          // Directory should show in filetree
-          expect($$('.tree-branch-head').count()).toBe(initialHeadCnt+1);
-          var dirName = dirList.last().element(
-            by.xpath('..'))
-            .$('span.filetable-filename');
-          browser.actions().doubleClick(dirList.last()).perform();
-          // Doubleclick should navigate to directory, and the
-          // breadcrumbs should reflect this
-          expect($$('ol.breadcrumb > li').last().getText())
-            .toBe(dirName.getText());
-          // Navigate back to root dir with the filetree
-          $$('.tree-label').first().click(function() {
-            dirList = $$('#dir-detail-table i.fa.fa-folder');
-            dirName = dirList.last().element(
-              by.xpath('..'))
-              .$('span.filetable-filename');
-            // Delete the directory
-            dirName.click(function() {
-              $('#delete-file-btn').click(function() {
-                expect($$('#dir-detail-table i.fa.fa-folder').count())
-                  .toBe(initialDirCnt);
+                expect(fileList.count()).toBe(13);
               });
             });
           });
