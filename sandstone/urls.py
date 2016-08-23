5d4208510361fcb22e0eb93bd7ca4d995ccc2d2e
Saurabh Sood <saurabhsood91@gmail.com>
2016-08-23 12:04:15 -0600

    rename handler

--- sandstone/urls.py
+++ sandstone/urls.py
@@ -2,7 +2,7 @@ from sandstone.lib.handlers.pam_auth import PAMLoginHandler
 from sandstone.lib.handlers.logout import LogoutHandler
 from sandstone.lib.handlers.main import MainHandler
 from sandstone.lib.handlers.state import StateHandler
-from sandstone.lib.handlers.update_handler import UpdateHandler
+from sandstone.lib.handlers.update import UpdateHandler
 from sandstone.lib.app_loader import DependencyHandler
 from sandstone.lib.app_loader import get_installed_app_urls
 
