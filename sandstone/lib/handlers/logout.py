517d5cf0d98b4192c306d07ce93d481bb2570c29
Zebula Sampedro <sampedro@colorado.edu>
2017-01-30 16:22:13 -0700

    Added logout button.

--- sandstone/lib/handlers/logout.py
+++ sandstone/lib/handlers/logout.py
@@ -6,4 +6,5 @@ class LogoutHandler(BaseHandler):
 
     def get(self):
         self.clear_cookie('user')
+        self.clear_cookie('_xsrf')
         self.redirect(self.get_argument('next', '/auth/login'))
