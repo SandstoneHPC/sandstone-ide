ca901c6f7858f76418abebd00092f5eea6dc465d
Zebula Sampedro <sampedro@colorado.edu>
2017-03-15 10:22:49 -0600

    Fixed rendering of breadcrumbs, added crumb selection.

--- sandstone/apps/filebrowser/static/details.controller.js
+++ sandstone/apps/filebrowser/static/details.controller.js
@@ -5,25 +5,40 @@ angular.module('sandstone.filebrowser')
 .controller('DetailsCtrl', ['$scope', '$modal', 'FilebrowserService', 'FilesystemService', function($scope,$modal,FilebrowserService,FilesystemService) {
   var self = this;
 
+  // Breadcrumbs
   self.breadcrumbs = [];
 
   var setBreadcrumbs = function() {
+    var volPath;
     if (!self.selection.volume) {
       // No volume selected yet
       self.breadcrumbs = [];
     } else if(!self.selection.cwd) {
       // Only a volume is selected
+      volPath = FilesystemService.normalize(self.selection.volume.filepath);
       self.breadcrumbs = [self.selection.volume.filepath];
     } else {
       var crumbs = [];
-      var volPath = FilesystemService.normalize(self.selection.volume.filepath);
+      volPath = FilesystemService.normalize(self.selection.volume.filepath);
       var cwdPath = FilesystemService.normalize(self.selection.cwd.filepath);
       var crumbPath = FilesystemService.normalize(cwdPath.replace(volPath,''));
       crumbs.push(volPath);
       var crumbCmps = FilesystemService.split(crumbPath).slice(1);
       for (var i=0; i<crumbCmps.length; i++) {
-        self.breadcrumbs.push(crumbCmps[i]);
+        crumbs.push(crumbCmps[i]);
       }
+      self.breadcrumbs = crumbs;
+    }
+  };
+
+  self.selectCrumb = function(index) {
+    if(index===0) {
+      self.changeDirectory(self.selection.volume.filepath);
+    } else {
+      var pathCmps = self.breadcrumbs.slice(0,index+1);
+      var navPath = FilesystemService.join.apply(this,pathCmps);
+      navPath = FilesystemService.normalize(navPath);
+      self.changeDirectory(navPath);
     }
   };
 
