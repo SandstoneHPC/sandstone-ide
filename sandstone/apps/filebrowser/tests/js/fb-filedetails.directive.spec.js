3e75d1be2248ca551d43e89c5f0bd3a09f0c3774
Zebula Sampedro <sampedro@colorado.edu>
2017-03-24 12:26:47 -0600

    Added more tests for fbFileDetails.

--- sandstone/apps/filebrowser/tests/js/fb-filedetails.directive.spec.js
+++ sandstone/apps/filebrowser/tests/js/fb-filedetails.directive.spec.js
@@ -3,40 +3,7 @@
 describe('sandstone.filebrowser.fbFileDetails', function() {
   var baseElement = '<div fb-file-details></div>';
 
-  var volumeDetails = {
-    available: '11G',
-    filepath: '/volume1/',
-    size: '18G',
-    type: 'volume',
-    used: '6.0G',
-    used_pct: 36
-  };
-  var fsDetails = {
-    groups: ['testgrp'],
-    type: 'filesystem',
-    volumes: [volumeDetails]
-  };
-  var fileDetails = {
-    type: 'file',
-    filepath: '/volume1/file1',
-    dirpath: '/volume1',
-    name: 'file1',
-    owner: 'testuser',
-    group: 'testgrp',
-    permissions: 'rwxrw-r--',
-    size: '8.8K'
-  };
-  var dirDetails = {
-    type: 'directory',
-    filepath: '/volume1',
-    dirpath: '/volume1/',
-    name: 'volume1',
-    owner: 'testuser',
-    group: 'testgrp',
-    permissions: 'rwxrw-r--',
-    size: '4.0K',
-    contents: [fileDetails]
-  };
+  var volumeDetails, fsDetails, fileDetails, dirDetails;
 
   var FilesystemService;
   var FilebrowserService;
@@ -49,6 +16,43 @@ describe('sandstone.filebrowser.fbFileDetails', function() {
   beforeEach(module('sandstone.broadcastservice'));
   beforeEach(module('sandstone.filesystemservice'));
   beforeEach(module('sandstone.filebrowser'));
+
+  beforeEach(function() {
+    volumeDetails = {
+      available: '11G',
+      filepath: '/volume1/',
+      size: '18G',
+      type: 'volume',
+      used: '6.0G',
+      used_pct: 36
+    };
+    fsDetails = {
+      groups: ['testgrp'],
+      type: 'filesystem',
+      volumes: [volumeDetails]
+    };
+    fileDetails = {
+      type: 'file',
+      filepath: '/volume1/file1',
+      dirpath: '/volume1',
+      name: 'file1',
+      owner: 'testuser',
+      group: 'testgrp',
+      permissions: 'rwxrw-r--',
+      size: '8.8K'
+    };
+    dirDetails = {
+      type: 'directory',
+      filepath: '/volume1',
+      dirpath: '/volume1/',
+      name: 'volume1',
+      owner: 'testuser',
+      group: 'testgrp',
+      permissions: 'rwxrw-r--',
+      size: '4.0K',
+      contents: [fileDetails]
+    };
+  });
 
   beforeEach(inject(function(_FilesystemService_,_$q_) {
     $q = _$q_;
@@ -170,7 +174,43 @@ describe('sandstone.filebrowser.fbFileDetails', function() {
       expect(isolateScope.editFile.name).toEqual(originalFilename);
     });
 
-    it('selecting a new group changes the group',function() {});
+    it('selecting a new group changes the group',function() {
+      var filepath = '/volume1/file1';
+      var group = 'testgrp2';
+      FilebrowserService.setSelectedFile(fileDetails);
+      $scope.$digest();
+
+      var deferredChangeGroup = $q.defer();
+      deferredChangeGroup.resolve();
+      spyOn(FilesystemService,'changeGroup').and.returnValue(deferredChangeGroup.promise);
+
+      isolateScope.editFile = {group: group};
+      isolateScope.changeGroup();
+      $scope.$digest();
+
+      expect(FilesystemService.changeGroup.calls.argsFor(0)).toEqual([filepath,group]);
+      expect(isolateScope.selection.selectedFile.group).toEqual(group);
+    });
+
+    it('group change error shows alert and resets group',function() {
+      var filepath = '/volume1/file1';
+      var originalGroup = fileDetails.group;
+      var group = 'testgrp2';
+      FilebrowserService.setSelectedFile(fileDetails);
+      $scope.$digest();
+
+      var deferredChangeGroup = $q.defer();
+      deferredChangeGroup.reject({},500);
+      spyOn(FilesystemService,'changeGroup').and.returnValue(deferredChangeGroup.promise);
+      spyOn(AlertService,'addAlert').and.returnValue('uuid-4444');
+
+      isolateScope.editFile = {group: group};
+      isolateScope.changeGroup();
+      $scope.$digest();
+
+      expect(AlertService.addAlert.calls.count()).toEqual(1);
+      expect(isolateScope.editFile.group).toEqual(originalGroup);
+    });
 
     it('editing permissions model changes permissions',function() {});
 
