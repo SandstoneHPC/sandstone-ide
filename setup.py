594e6855ee2f4924cac6e8905362ed13e0af2ba8
Zebula Sampedro <sampedro@colorado.edu>
2017-04-13 10:19:01 -0600

    Specify Tornado version.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'peewee',
         'simplepam',
-        'tornado>=4',
+        'tornado==4.3',
         'terminado',
         'watchdog',
         'cerberus',
