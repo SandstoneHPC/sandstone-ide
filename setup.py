90a60d4e5e791fcc00075e86549ddee1ccfa4be9
Zebula Sampedro <sampedro@colorado.edu>
2017-05-03 14:28:04 -0600

    Bumped to v1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='sandstone',
-    version='1.0.0',
+    version='1.0.1',
     author=u'Zebula Sampedro',
     author_email='sampedro@colorado.edu',
     packages=find_packages(),
